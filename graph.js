b0ea4a07bf000d9a0b76fe3dc7c4e846704e15c5
Sydney Seraphim <sydney.seraphim@hotmail.com>
2018-03-28 15:32:53 -0700

    Two stars with random positions

--- graph.js
+++ graph.js
@@ -1,18 +1,23 @@
 // Where is the circle
-var x, y;
+var x1, x2, y1, y2, r1, r2;
+var m1 = 10;
+var m2 = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(15);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
+  
+  //starting positions
+  x1 = width / 2;
+  y1 = height / 2;
+  x2 = width / 2;
+  y2 = height;
 
 
   // create sliders
-  sliderM1 = createSlider(0, 255, 100);
+  sliderM1 = createSlider(0, 200, 100);
   sliderM1.position(20, 20);
-  sliderM2 = createSlider(0, 255, 0);
+  sliderM2 = createSlider(0, 200, 100);
   sliderM2.position(20, 50);
 }
 
@@ -20,23 +25,32 @@ function draw(){
   background(0);
 
   //slider values
-  var m1 = sliderM1.value();
-  var m2 = sliderM2.value();
+  m1 = sliderM1.value();
+  m2 = sliderM2.value();
+
+  //change size of stars according to mass
+  r1 = 5 * sqrt(m1);
+  r2 = 5 * sqrt(m2);
+
+  //slider names
   text("Mass 1", sliderM1.x * 2 + sliderM1.width, 35);
   text("Mass 2", sliderM2.x * 2 + sliderM2.width, 65);
   
-  // Draw a circle
+  //draw the stars
   noStroke();
   fill(255);
-  ellipse(x, y, 24, 24);
+  ellipse(x1, y1, r1, r1);
+  ellipse(x2, y2, r2, r2);
   
-  // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
-  
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
-  }
+  //update horizontal position
+  x1 = x1 + random(-1, 1);
+  x2 = x2 + random(-1, 1);
+  //update vertical position
+  y1 = y1 - 1;
+  y2 = y2 - 1;
+
+  //draw the center of mass
+  stroke(127);
+  line(width/2.-3, height/2., width/2.+3, height/2.);
+  line(width/2., height/2.-3, width/2., height/2.+3);
 }
