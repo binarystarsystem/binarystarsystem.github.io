e48943f70b5f100c58ba77e3424dd6b47712ad3d
Toren Dofher <tdofher@gmail.com>
2018-04-01 17:02:20 -0700

    N bodies woot

--- orbit.js
+++ orbit.js
@@ -15,8 +15,14 @@ var G = 100000;
 var AU = 100;
 var console_flag = 0;
 
-
-
+//these arrays are how the N bodies are implemented
+var masses = [];
+var positions;
+var velocities;
+var accelerations;
+var stars = [];
+var sliders = [];
+var N = 3;
 
 
 //var G = 6.674*pow(10,-11);
@@ -30,22 +36,19 @@ function setup() {
   //create canvas
   createCanvas(windowWidth, windowHeight);
   textSize(15);
-    //document.write(windowHeight + " " + windowWidth);
-  //create sliders
-  sliderM1 = createSlider(0, 100, 25);
-  sliderM1.position(20, 20);
-  sliderM2 = createSlider(0, 100, 25);
-  sliderM2.position(20, 50);
-  sliderE = createSlider(0, 100, 50);
-  sliderE.position(20, 80);
-  sliderI = createSlider(0, 90, 0);
-  sliderI.position(20, 110);
 
-  //slider values
-  m1 = sliderM1.value();
-  m2 = sliderM2.value();
-  e = sliderE.value()/100;
-  i = sliderI.value();
+    for (var index = 0; index < N; index++) {
+        slider = createSlider(0,100,10);
+        sliders.push(slider);
+        sliders[index].position(20, 20 + 30 * index);   
+    }
+    for (var index = 0; index < N; index++) {       
+        masses.push(sliders[index].value());
+    }
+
+    
+    e = 1;//sliderE.value()/100;
+    i = 1;//sliderI.value();
   r1 = createVector(0.2*AU, 0, 0);
   r2 = createVector(-0.2*AU, 0, 0);  
   v1 = createVector(0, 10, 0);
@@ -54,92 +57,68 @@ function setup() {
   a2 = createVector(0, 0, 0);
     
 
-  star1 = new Star(m1, norma, e, 0);
-  star2 = new Star(m2, norma, e, PI);
+    
+    positions = [r1, r2, createVector(0,0,0)];
+    velocities = [v1, v2, createVector(0,0,0)];
+    accelerations = [a1, a2, createVector(0, 0, 0)];
+
+    for (var index = 0; index < N; index++) {
+        star = new Star(masses[index], norma, e, PI);
+        stars.push(star);
+    }
 }
 
 function draw(){
   background(0);
 
   //check if slider values have changed
-  if (sliderM1.value() != m1 || sliderM2.value() != m2 || sliderE.value() != e || sliderI.value() != i) {
-    create_binary(sliderM1.value(), sliderM2.value());
-    m1 = sliderM1.value();
-    m2 = sliderM2.value();
-    e = sliderE.value()/100;
-    i = sliderI.value();
-  }
+    for (var index = 0; index < N; index++) {
+        if (sliders[index].value() != masses[index]) {
+            masses[index] = sliders[index].value();
+            stars[index].m = masses[index];
+            stars[index].r = 5*sqrt(masses[index]);
+        }
+        //slider names
+        text("Mass " + (index+1), sliders[index].x * 2 + sliders[index].width, 35 + 30 * index);
+    }
 
   noStroke();
   fill(220);
 
-  //slider names
-  text("mass 1", sliderM1.x * 2 + sliderM1.width, 35);
-  text("mass 2", sliderM2.x * 2 + sliderM2.width, 65);
-  text("eccentricity", sliderE.x * 2 + sliderE.width, 95);
-  text("inclination", sliderI.x * 2 + sliderI.width, 125);
+    //the primary physics engine generalized for N bodies
+    var positions_copy = positions;
+    var velocities_copy = velocities;
 
- 
-    r1.x = r1.x + v1.x * dt;
-    r2.x = r2.x + v2.x * dt;
-    r1.y = r1.y + v1.y * dt;
-    r2.y = r2.y + v2.y * dt;
-    a1.x = G*m2 * m1 * (r2.x - r1.x) / pow(sqrt(pow(r2.x - r1.x, 2) + pow(r2.y - r1.y, 2)), 3) / 10e3;
-    a1.y = G*m2 * m1 * (r2.y - r1.y) / pow(sqrt(pow(r2.x - r1.x, 2) + pow(r2.y - r1.y, 2)), 3) / 10e3;
-    v1.x = v1.x + a1.x * dt;
-    v1.y = v1.y + a1.y * dt;
-    a2.x = G*m2 * m1 * (r1.x - r2.x) / pow(sqrt(pow(r2.x - r1.x, 2) + pow(r2.y - r1.y, 2)), 3) / 10e3;
-    a2.y = G*m2 * m1 * (r1.y - r2.y) / pow(sqrt(pow(r2.x - r1.x, 2) + pow(r2.y - r1.y, 2)), 3) / 10e3;
-    v2.x = v2.x + a2.x * dt;
-    v2.y = v2.y + a2.y * dt;
+    for (var primary = 0; primary < masses.length; primary++) {
+        positions[primary].x = positions_copy[primary].x + velocities_copy[primary].x * dt;
+        positions[primary].y = positions_copy[primary].y + velocities_copy[primary].y * dt;
+        accelerations[primary].x = 0;
+        accelerations[primary].y = 0;
+        for (var secondary = 0; secondary < masses.length; secondary++) {
+            if (primary != secondary) {
+                norm2 = pow(sqrt(pow(positions_copy[secondary].x - positions_copy[primary].x, 2) + pow(positions_copy[secondary].y - positions_copy[primary].y, 2)), 3) ;
+                accelerations[primary].x += G * masses[primary] * masses[secondary] * (positions_copy[secondary].x - positions_copy[primary].x) / norm2 / 10e3;
+                accelerations[primary].y += G * masses[primary] * masses[secondary] * (positions_copy[secondary].y - positions_copy[primary].y) / norm2 / 10e3;
+            }     
+        }
+        velocities[primary].x = velocities_copy[primary].x + accelerations[primary].x * dt;
+        velocities[primary].y = velocities_copy[primary].y + accelerations[primary].y * dt;
 
-
-    /*if (abs(a1.x) > 10 || abs(a1.y) > 10 || abs(a2.x) > 10 || abs(a2.y) > 10) {
-        r1.x = 0;
-        r2.x = 0;
-        r1.y = 0;
-        r2.y = 0;
-        a1.x = 0;
-        a1.y = 0;
-        v1.x = 0;
-        v1.y = 0;
-        a2.x = 0;
-        a2.y = 0;
-        v2.x = 0;
-        v2.y = 0;
-    }*/
-
-    if (console_flag % 10 == 0) {
-        console.log("r1x " + r1.x);
-        console.log("r2 " + r2.x);
-        console.log("v1 " + v1.x);
-        console.log("v2 " + v2.x);
-        console.log("a1 " + a1.x);
-        console.log("a2 " + a2.x);
-        console.log("r1y " + r1.y);
-        console.log("r2 " + r2.y);
-        console.log("v1 " + v1.y);
-        console.log("v2 " + v2.y);
-        console.log("a1 " + a1.y);
-        console.log("a2 " + a2.y);
     }
-   
+ 
     
+    //update the positions and draw stars
+    for (var index = 0; index < N; index++) {
+        stars[index].update_position(positions[index]);
+        stars[index].draw_path();
+        stars[index].draw();
+    }
     
+//for troubleshooting
+    if (console_flag % 10 == 0) {
+        display_console; 
+    }
     
-    
-    //update the positions
-    star1.update_position(r1);
-    star2.update_position(r2);
-
-    //document.write(star1.pos.x + " " + star2.pos.x);
-
-  //draw the stars
-  star1.draw_path();
-  star2.draw_path();
-  star1.draw();
-  star2.draw();
-
   //draw the center of mass
   stroke(127);
   line(width/2.-3, height/2., width/2.+3, height/2.);
@@ -150,7 +129,20 @@ function draw(){
     console_flag += 1;
 }
 
-
+function display_console() {
+    console.log("r1x " + r1.x);
+    console.log("r2 " + r2.x);
+    console.log("v1 " + v1.x);
+    console.log("v2 " + v2.x);
+    console.log("a1 " + a1.x);
+    console.log("a2 " + a2.x);
+    console.log("r1y " + r1.y);
+    console.log("r2 " + r2.y);
+    console.log("v1 " + v1.y);
+    console.log("v2 " + v2.y);
+    console.log("a1 " + a1.y);
+    console.log("a2 " + a2.y);
+}
 
 function Star(m_, a_, e_, tau_) {
   this.pos = createVector(0, 0, 0);
@@ -183,17 +175,12 @@ function Star(m_, a_, e_, tau_) {
     }
   };
 
-    this.update_position = function (position) {
-    //find the eccentric anomaly from the time
-   // var E = this.t_to_E(t);
-    //the orbit mechanics happens here
-      //this.pos.x = cos(this.tau)*this.a*(cos(t/24*PI)-this.e);
-     // this.pos.y = (sin(i*PI/180))*cos(this.tau)*this.a*(sqrt(1-pow(this.e, 2))*sin(t/24*PI));
-      
-      //add the current position to the path array
+    this.update_position = function (position) { 
+
+        //add the current position to the path array
         this.pos = position;
         this.path.push(this.pos.copy());
-        //document.write(" (" + this.pos.x + " , " + this.pos.y + ") ");
+       
     };
 
     this.update_velocity = function (velocity) {
