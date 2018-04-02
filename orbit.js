fe48a4c7f75d8f0ff6968b38b2ba18ce9ce5daa4
Toren Dofher <tdofher@gmail.com>
2018-04-01 17:50:10 -0700

    Reset working, fully generalized to variable N initial conditions need work still

--- orbit.js
+++ orbit.js
@@ -1,71 +1,58 @@
-var star1;
-var star2;
-var m1 = 25;
-var m2 = 25;
+
+
 var e = 0.5;
 var i = 0;
 var norma = 200;
-var r1;
+/*var r1;
 var r2;
 var v1;
 var v2;
 var a1;
-var a2;
+var a2;*/
 var G = 100000;
 var AU = 100;
-var console_flag = 0;
 
 //these arrays are how the N bodies are implemented
 var masses = [];
-var positions;
-var velocities;
-var accelerations;
+var positions = [];
+var velocities = [];
+var accelerations = [];
 var stars = [];
 var sliders = [];
 var N = 3;
 
 
-//var G = 6.674*pow(10,-11);
-//var AU = 1.50e11; //150 million km = 150 billion m
-//var M = 1.98892e30;
-
 var t = 0;
 var dt = 0.1;
+var console_flag = 0;
+var setup_flag = 0
+
 
 function setup() {
   //create canvas
-  createCanvas(windowWidth, windowHeight);
-  textSize(15);
 
+    
+        createCanvas(windowWidth, windowHeight);
+        textSize(15);
+
+        for (var index = 0; index < N; index++) {
+            slider = createSlider(0, 100, 10);
+            sliders.push(slider);
+            sliders[index].position(20, 20 + 30 * index);
+        }
+        for (var index = 0; index < N; index++) {
+            masses.push(sliders[index].value());
+        }
+
+    //creates stars and sets initial conditions
     for (var index = 0; index < N; index++) {
-        slider = createSlider(0,100,10);
-        sliders.push(slider);
-        sliders[index].position(20, 20 + 30 * index);   
-    }
-    for (var index = 0; index < N; index++) {       
-        masses.push(sliders[index].value());
-    }
-
-    
-    e = 1;//sliderE.value()/100;
-    i = 1;//sliderI.value();
-  r1 = createVector(0.2*AU, 0, 0);
-  r2 = createVector(-0.2*AU, 0, 0);  
-  v1 = createVector(0, 10, 0);
-  v2 = createVector(0, -10, 0);
-  a1 = createVector(0, 0, 0);
-  a2 = createVector(0, 0, 0);
-    
-
-    
-    positions = [r1, r2, createVector(0,0,0)];
-    velocities = [v1, v2, createVector(0,0,0)];
-    accelerations = [a1, a2, createVector(0, 0, 0)];
-
-    for (var index = 0; index < N; index++) {
+        positions.push(createVector(cos(2 * PI * index / N) * 0.6 * AU, sin(2 * PI * index / N) * 0.6 * AU, 0));
+        velocities.push(createVector(cos(2 * PI * index / N) * 2, sin(2 * PI * index / N) * -2, 0));
+        accelerations.push(createVector(0, 0, 0));
         star = new Star(masses[index], norma, e, PI);
         stars.push(star);
     }
+    setup_flag = 1;
 }
 
 function draw(){
@@ -76,7 +63,8 @@ function draw(){
         if (sliders[index].value() != masses[index]) {
             masses[index] = sliders[index].value();
             stars[index].m = masses[index];
-            stars[index].r = 5*sqrt(masses[index]);
+            stars[index].r = 5 * sqrt(masses[index]);
+            reset();
         }
         //slider names
         text("Mass " + (index+1), sliders[index].x * 2 + sliders[index].width, 35 + 30 * index);
@@ -101,8 +89,19 @@ function draw(){
                 accelerations[primary].y += G * masses[primary] * masses[secondary] * (positions_copy[secondary].y - positions_copy[primary].y) / norm2 / 10e3;
             }     
         }
+        //fix singularities here by modifying dt
+        if (accelerations[primary].x > 10 || accelerations[primary].y > 10) {
+            dt = 0.0001;
+        }
+        else {
+            dt = 0.1;
+        }
         velocities[primary].x = velocities_copy[primary].x + accelerations[primary].x * dt;
         velocities[primary].y = velocities_copy[primary].y + accelerations[primary].y * dt;
+        dt = 0.1;
+        if (abs(positions[primary].x) > windowWidth/2 || abs(positions[primary].y) > windowHeight/2) {
+            reset()
+        }
 
     }
  
@@ -119,29 +118,17 @@ function draw(){
         display_console; 
     }
     
-  //draw the center of mass
-  stroke(127);
-  line(width/2.-3, height/2., width/2.+3, height/2.);
-  line(width/2., height/2.-3, width/2., height/2.+3);
+    stroke(127);
 
   //update time
     t += dt;
     console_flag += 1;
+
+    
 }
 
 function display_console() {
-    console.log("r1x " + r1.x);
-    console.log("r2 " + r2.x);
-    console.log("v1 " + v1.x);
-    console.log("v2 " + v2.x);
-    console.log("a1 " + a1.x);
-    console.log("a2 " + a2.x);
-    console.log("r1y " + r1.y);
-    console.log("r2 " + r2.y);
-    console.log("v1 " + v1.y);
-    console.log("v2 " + v2.y);
-    console.log("a1 " + a1.y);
-    console.log("a2 " + a2.y);
+    //console.log();
 }
 
 function Star(m_, a_, e_, tau_) {
@@ -176,20 +163,20 @@ function Star(m_, a_, e_, tau_) {
   };
 
     this.update_position = function (position) { 
-
         //add the current position to the path array
         this.pos = position;
-        this.path.push(this.pos.copy());
-       
+        this.path.push(this.pos.copy());     
     };
-
-    this.update_velocity = function (velocity) {
-
-    }
-
-
 }
 
+function reset() {
+    for (var index2 = 0; index2 < N; index2++) {
+                positions[index2]=createVector(cos(2 * PI * index2 / N) * 0.6 * AU, sin(2 * PI * index2 / N) * 0.6 * AU, 0);
+                velocities[index2] = createVector(cos(2 * PI * index2 / N) * 2, sin(2 * PI * index2 / N) * -2, 0);
+                accelerations[index2] = createVector(0, 0, 0);
+                stars[index2].path = [];
+      }
+}
 
 function create_binary(m1, m2) {
     // determine the semi-major axes so that the larger of the two is equal to the variable norma;
